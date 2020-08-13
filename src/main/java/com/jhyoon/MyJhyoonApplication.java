1876ceb4db181965cd2efd63d695c1c03c5c2a41
jeonghyeockYoon <zlal0807@gmail.com>
2020-08-13 15:00:45 +0900

    init

---
+++ src/main/java/com/jhyoon/MyJhyoonApplication.java
@@ -0,0 +1,13 @@
+package com.jhyoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJhyoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyJhyoonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhyoon/MyJhyoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhyoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyJhyoonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
