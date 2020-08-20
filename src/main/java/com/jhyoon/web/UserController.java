9f3f867fb3183f377025b76630253aa72e570a7a
jeonghyeockYoon <zlal0807@gmail.com>
2020-08-20 14:30:09 +0900

    개인정보 수정까지

--- src/main/java/com/jhyoon/web/UserController.java
+++ src/main/java/com/jhyoon/web/UserController.java
@@ -1,32 +1,99 @@
 package com.jhyoon.web;
 
-import java.util.ArrayList;
-import java.util.List;
+import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.jhyoon.domain.User;
+import com.jhyoon.domain.UserRepository;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
-	
-	private List<User> users = new ArrayList<User>();
 
-	@PostMapping("/create")
+	@Autowired
+	private UserRepository userRepository;
+
+	@GetMapping("/form")
+	public String form() {
+		return "/user/form";
+	}
+
+	@GetMapping("/loginForm")
+	public String loginForm() {
+		return "/user/login";
+	}
+	
+	@PostMapping("login")
+	public String login(String userId, String password, HttpSession session) {
+		
+		User user = userRepository.findByUserId(userId);
+		
+		String returnFailed = "redirect:/users/loginForm";
+		if(user == null) return returnFailed;
+		if(!password.equals(user.getPassword())) return returnFailed;
+		
+		session.setAttribute("sessionedUser", user);
+		
+		System.out.println("loginSuccess!");
+		return "redirect:/";
+	}
+	
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		session.removeAttribute("sessionedUser");
+		return "redirect:/";
+	}
+
+	@PostMapping("")
 	public String createUser(User user) {
-		System.out.println("user : "+user);
-		users.add(user);
-		return "redirect:/list";
+		userRepository.save(user);
+		return "redirect:/users";
 	}
-	
-	
-	@GetMapping("/list")
+
+	@GetMapping("")
 	public String list(Model model) {
-		model.addAttribute("users",users);
-		return "list";
+		model.addAttribute("users", userRepository.findAll());
+		return "/user/list";
 	}
-	
-	
-	
+
+	@GetMapping("{seq}/form")
+	public String updateForm(@PathVariable Long seq, Model model, HttpSession session) {
+		
+		User sessionedUser = (User) session.getAttribute("sessionedUser");
+		
+		if(sessionedUser == null) return "redirect:/users/loginForm";
+		
+		if(!sessionedUser.getSeq().equals(seq)) {
+			throw new IllegalStateException("본인정보만 수정 가능합니다.");
+		}
+
+		model.addAttribute("user", userRepository.findById(seq).get());
+		return "/user/updateForm";
+	}
+
+	@PutMapping("/{seq}")
+	public String update(@PathVariable Long seq, User updatedUser, HttpSession session) {
+		
+		User sessionedUser = (User) session.getAttribute("sessionedUser");
+		
+		if(sessionedUser == null) return "redirect:/users/loginForm";
+		
+		if(!sessionedUser.getSeq().equals(seq)) {
+			throw new IllegalStateException("본인정보만 수정 가능합니다.");
+		}
+		
+		User user = userRepository.findById(seq).get();
+		user.update(updatedUser);
+		userRepository.save(user);
+		return "redirect:/users";
+	}
+
 }
---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO USER(USER_ID, PASSWORD, NAME, EMAIL) VALUES ('user1','1234','user1', 'user1@gmail.com');
+INSERT INTO USER(USER_ID, PASSWORD, NAME, EMAIL) VALUES ('user2','1234','user2', 'user2@gmail.com');
+INSERT INTO USER(USER_ID, PASSWORD, NAME, EMAIL) VALUES ('user3','1234','user3', 'user3@gmail.com');
